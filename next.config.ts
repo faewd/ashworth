982fac16bc1d96dfef40a363bdee34625f45b2b6
faewd <git@faew.dev>
2025-05-01 21:36:43 +0100

    Allow rendering discord profile pictures

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,12 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
+        hostname: "cdn.discordapp.com",
+        port: "",
+        pathname: "**"
+      },
+      {
+        protocol: "https",
         hostname: "placehold.co",
         port: "",
         pathname: "**"
